ed4451da881c929ec7f24486cd99cbb4de871bb0
lam <lamhoangpham20@gmail.com>
2022-10-13 13:01:10 +0300

    add api call to front end

--- web/next-mui/pages/components/Booking.js
+++ web/next-mui/pages/components/Booking.js
@@ -2,23 +2,40 @@ import * as React from "react";
 import { TextField, Paper, InputBase, IconButton, Box } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import BookingItems from "./BookingItems";
+import axios, * as others from "axios";
 
 export default function Booking() {
   const [input, setInput] = React.useState("");
-  
+  const [data, setData] = React.useState(null);
+  const [page, setPage] = React.useState(1);
   const submitEmail = async () => {
-    const res = await fetch(
-      `https://booking-app-24-01.herokuapp.com/booking/email?email=${input}`
-    )
+    const res = await axios
+      .get(
+        `https://booking-app-24-01.herokuapp.com/booking/email?email=${input}&page=${page}`
+      )
       .then((result) => {
-        console.log(result)
-        return result.data;
+        console.log(result.data);
+        setData(result.data);
       })
       .catch((err) => {
-        console.log(error);
+        console.log(err);
       });
   };
 
+  const changePage = ()=>{
+    setPage(page+1)
+  }
+  const bookingItems = () => {
+    if (data !== null) {
+      if (Array.isArray(data)) {
+        return <BookingItems data={data} changePage={changePage} page={page} />;
+      } else {
+        return <div>{data.message}</div>;
+      }
+    } else {
+      return <></>;
+    }
+  };
   return (
     <>
       <Paper
@@ -42,9 +59,7 @@ export default function Booking() {
           <SearchIcon />
         </IconButton>
       </Paper>
-      <Box sx={{ marginTop: "2em" }}>
-        <BookingItems />
-      </Box>
+      <Box sx={{ marginTop: "2em" }}>{bookingItems()}</Box>
     </>
   );
 }
--- web/next-mui/pages/components/BookingItems.js
+++ web/next-mui/pages/components/BookingItems.js
@@ -1,5 +1,21 @@
 import * as React from "react";
-
-export default function BookingItems() {
-    return(<></>)
+import { Container, Button } from "@mui/material";
+import CardBox from "./CardBox";
+export default function BookingItems(props) {
+  return (
+    <>
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        {props.data.map((i) => {
+          return (
+            <div key={i.booking_id}>
+              <CardBox item={i} />
+            </div>
+          );
+        })}
+        <Button>{"<"}</Button>
+        {props.page}
+        <Button onClick={props.changePage}>{">"}</Button>
+      </Container>
+    </>
+  );
 }
